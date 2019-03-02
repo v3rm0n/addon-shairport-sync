80aaee82f8b0693e51a65c1c37abe93799370a4c
Maido Käära <m@maido.io>
2019-03-03 00:30:15 +0200

    Using prebuilt images

--- rootfs/etc/cont-init.d/20-dbus.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bash
-# ==============================================================================
-# Community Hass.io Add-ons: Homebridge
-# Ensures we've got an unique D-Bus ID
-# ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
-dbus-uuidgen --ensure || hass.die 'Failed to generate a unique D-Bus ID'
